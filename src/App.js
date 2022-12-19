2edc87940e5ead73eb23395663b9cef508e08db1
Malek Mneri <mnerimalek@gmail.com>
2022-12-19 19:34:35 +0100

    a basic example of storybook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Button from './Components/Button'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-center">
+      <br />
+      <br />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <br />
+      <br />
+      <Button label="Start" backgroundColor="skyblue" />
     </div>
   )
 }
---
+++ src/Components/Button.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Button({ label, backgroundColor = 'red', size = 'md' }) {
+  let scale = 1
+  if (size === 'sm') scale = 0.75
+  if (size === 'lg') scale = 1.5
+  const style = {
+    backgroundColor,
+    padding: `${scale * 0.5}rem ${scale * 1}rem`,
+    border: 'none',
+  }
+  return <button style={style}>{label}</button>
+}
+
+Button.propTypes = {
+  label: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  size: PropTypes.oneOf(['sm', 'md', 'lg']),
+}
+
+export default Button
