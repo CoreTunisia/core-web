3a14cd57a2c3a90fbb1e4fe126ab47ded5985936
JOUINI Boubaker <boubaker.jouini@esprit.tn>
2022-12-19 16:47:10 +0100

    add & configured tailwind css + deleted css + better code for eslint

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      minWidth: {
+        0: '0',
+        '1/5': '20%',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        full: '100%',
+      },
+      minHeight: {
+        '82vh': '82vh',
+        '90vh': '90vh',
+      },
+      flex: {
+        2: '2 2 0%',
+        3: '3 3 0%',
+        4: '4 4 0%',
+        5: '5 5 0%',
+      },
+      colors: {
+        'second-400': 'rgba(48,53,114,0.7)',
+        second: 'rgb(28,33,94)',
+        'second-600': 'rgba(08,13,74)',
+        main: 'rgb(233,58,100)',
+        'main-600': 'rgb(213,38,80)',
+        'main-100': 'rgb(233,58,100, 0.3)',
+      },
+    },
+  },
+  plugins: [],
+}
