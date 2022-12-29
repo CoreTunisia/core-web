2cb6dbb961c7d7956cf50c80709c3f1d3d0a4c2e
Malek Mneri <mnerimalek@gmail.com>
2022-12-29 18:27:07 +0100

    sign up page & routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Login from './Pages/Auth/Login/index.js'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/home/index.js'
+import SignUp from './Pages/Auth/Signup/index.js'
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Home />} />
     </Routes>
--- src/Pages/Auth/Login/index.js
+++ src/Pages/Auth/Login/index.js
@@ -81,7 +81,7 @@ export default function Login() {
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet?{' '}
-                <a href="#" className="font-medium text-primary hover:underline dark:text-primary-500">
+                <a href="/signup" className="font-medium text-primary hover:underline dark:text-primary-500">
                   Sign up
                 </a>
               </p>
